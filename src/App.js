4690195f2c34393da8c43055c4b332792837a165
Jorge <jorgediazok@gmail.com>
2021-07-20 00:34:36 -0300

    First commit. App setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//ROUTING
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BLOG</h1>
+    </div>
+  );
+}
+
+export default App;
